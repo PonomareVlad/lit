f55a031dfab4513e1f9cab06fb85e5a4d6160aa2
PonomareVlad <Git@PonomareVlad.ru>
2022-11-20 05:53:23 +0500

    @lit-async/lit-element@3.2.2-mirror.4

--- packages/lit-element/src/lit-element.ts
+++ packages/lit-element/src/lit-element.ts
@@ -300,7 +300,7 @@ export const _$LE = {
 
 // IMPORTANT: do not change the property name or the assignment expression.
 // This line will be used in regexes to search for LitElement usage.
-(globalThis.litElementVersions ??= []).push('3.2.2-mirror.3');
+(globalThis.litElementVersions ??= []).push('3.2.2-mirror.4');
 if (DEV_MODE && globalThis.litElementVersions.length > 1) {
   issueWarning!(
     'multiple-versions',
