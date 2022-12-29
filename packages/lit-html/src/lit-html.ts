cac2684eba4a1f4816e95d9756163e87914f4a67
Vladislav Ponomarev <Git@PonomareVlad.ru>
2022-12-30 03:25:30 +0500

    @worklit/lit-html@2.5.0-worklit.4 (modified global)

--- packages/lit-html/src/lit-html.ts
+++ packages/lit-html/src/lit-html.ts
@@ -2098,7 +2098,7 @@ polyfillSupport?.(Template, ChildPart);
 
 // IMPORTANT: do not change the property name or the assignment expression.
 // This line will be used in regexes to search for lit-html usage.
-(global.litHtmlVersions ??= []).push('2.5.0-worklit.2');
+(global.litHtmlVersions ??= []).push('2.5.0-worklit.4');
 if (DEV_MODE && global.litHtmlVersions.length > 1) {
   issueWarning!(
     'multiple-versions',
