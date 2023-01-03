2dc56077193194f189b487da5f5616e9c819c9e3
Vladislav Ponomarev <Git@PonomareVlad.ru>
2023-01-03 12:16:09 +0500

    Added patch script

---
+++ scripts/patch.js
@@ -0,0 +1,36 @@
+import fs from "node:fs";
+
+const files = [
+  "packages/lit-html/lit-html.js",
+  "packages/lit-html/node/lit-html.js"
+];
+
+const getURL = path =>
+  new URL(path, new URL(`../`, import.meta.url));
+
+const replacer = path => {
+  const url = getURL(path);
+  const initialSource = fs.readFileSync(url).toString();
+  let source = initialSource;
+  switch (path) {
+    case "packages/lit-html/lit-html.js":
+      source = source.replace(
+        `const i=window,{document:s}=i`,
+        `const i=globalThis,{document:s,window}=i`
+      );
+      break;
+    case "packages/lit-html/node/lit-html.js":
+      source = source.replace(
+        `const i=globalThis,{document:s}=i`,
+        `const i=globalThis,{document:s,window}=i`
+      );
+      break;
+    default:
+      return;
+  }
+  if (source === initialSource) return;
+  fs.writeFileSync(url, source);
+  console.log("Patched", path);
+};
+
+files.forEach(replacer);
